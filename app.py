6f761c47065e1d58314d312bba875433fb689e4c
mhnasser <mohamad.nasser@outlook.com.br>
2023-01-19 19:26:32 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+# Criar primeira página
+@app.route("/")
+def pag_inicial():
+
+    return render_template("index.html")
+
+@app.route("/pedido")
+def pag_pedido():
+
+    return render_template("pedido.html")
+
+@app.route("/pergunta")
+def pag_pergunta():
+
+    return render_template("pergunta.html")
+
+# Criar a segunda página
+
+# Colocar o site no ar
+app.run(debug=True)
+
